90c736072e67d6f651bafa4695346b615a531c36
Jacqueline <jcasalia@yahoo.com.br>
2025-02-25 14:34:10 -0300

    play e cronometro atualizando

--- frontend/src/components/player/Player.jsx
+++ frontend/src/components/player/Player.jsx
@@ -1,4 +1,4 @@
-import React, { useState } from 'react'
+import React, { useEffect, useState } from 'react'
 import "./Player.css"
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
 import { faBackwardStep, 
@@ -7,18 +7,42 @@ import { faBackwardStep,
 import { Link } from 'react-router-dom'
 import { useRef } from 'react'
 
+const formtTime=(timeInSeccond)=>{
+  // Math.floor arredonda
+  // .toString().padStart(2,0) formatando com duas casas
+  const minutes=Math.floor(timeInSeccond/60).toString().padStart(2,"0")
+  const seconds=Math.floor(timeInSeccond-minutes*60).toString().padStart(2,"0")
+return `${minutes}:${seconds}` 
 
+}
 
 export default function Player({duration,randomIdFormatArt,randomId2FormatArt,audio}) {
  const audioPlayer=useRef();
  const [isPlay,setIsPlayer]=useState(false)
-//  console.log(audioPlayer,current)
+const [currentTime,setCurrentTime]=useState(formtTime(0))
+
  
  const playPause=()=>{
   // esta tocando 
   isPlay? audioPlayer.current.pause():audioPlayer.current.play();
   setIsPlayer(!isPlay)
+
  }
+// rodar o conometro 
+useEffect(()=>{
+  const intervalId =  setInterval(()=>{
+  // atualizar o cronometro em qual momento da musica esta 
+
+  setCurrentTime(formtTime(audioPlayer.current.currentTime))
+  },1000)
+  //limpar
+  return()=>clearInterval(intervalId)
+  
+},[isPlay])
+
+
+
+
   return (
     <div className='player'>
    <div className='player__controllers'>
@@ -45,7 +69,7 @@ export default function Player({duration,randomIdFormatArt,randomId2FormatArt,au
 
    </div>
    <div className='player__progress'>
-<p >00:00</p>
+<p >{currentTime}</p>
 <div className='player__bar'>
     <div className='player__bar-progress'></div>
 </div>
